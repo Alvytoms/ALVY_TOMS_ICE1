93d81e092b17242d26c2a3a369bc15a01f8de1ac
alvyt <alvyt@10.0.0.37>
2024-05-28 18:37:38 -0400

    version1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,8 @@
  */
 package card;
 
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,18 +17,49 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random rand = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(rand.nextInt(13)+1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i]= c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner scanner = new Scanner(System.in);
+        System.out.printIn("enter the value ofthe card");
+        int Uservalue = scanner.nextInt();
+        Scanner.Nextline();
+        System.out.printIn("enter the suitof the card");
+        String Usersuit = scanner.Nextline();
+        
+        
         // and search magicHand here
+        boolean found = false;
+        for(Card card : Magichand)
+        {
+            if(card.getvalue()==Usercard.getvalue()&&card.getSuit())
+            {
+                found = true;
+                break;
+            }
+        }
         //Then report the result here
+        if (found)
+        {
+            system.out.printIn("card is in the magic hand");
+        }
+        else
+        {
+            system.out.printIn("card is not in magic hand");
+            
+        }
         // add one luckcard hard code 2,clubs
+        Card luckycard = new Card();
+        luckycard.SetValue(2);
+        luckycard.SetSuit("clubs");
     }
     
 }
